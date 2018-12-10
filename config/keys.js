e8401835f19fa45f39c58e6ade734dce017b7b0f
Rakesh E Pandu <rakeshpandu17@gmail.com>
2018-12-09 18:23:53 -0800

    FinalCommit

--- config/keys.js
+++ config/keys.js
@@ -2,5 +2,18 @@ module.exports = {
 	mongoURI:`mongodb://promedia:${encodeURIComponent('P@ssw0rd')}@ds038888.mlab.com:38888/professionalmedia`,
 	secretOrKey: 'secret'
 	}
+
+//module.exports = {
+//	mongoURI:`mongodb://kaladmin:${encodeURIComponent('P@ssw0rd!')}//@ds018558.mlab.com:18558/devconnector`,
+//	secretOrKey: 'secret'
+//	}
+
+
+//module.exports = {
+//	mongoURI:`mongodb://promedia:${encodeURIComponent('P@ssw0rd')}//@ds038888.mlab.com:38888/professionalmedia`,
+//	secretOrKey: 'secret'
+//	}
+
+
 //
 // mongoURI:`mongodb://promedia:${encodeURIComponent('P@ssw0rd!')}@ds038888.mlab.com:38888/professionalmedia`,
